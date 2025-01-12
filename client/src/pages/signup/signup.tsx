d9ff9330f26a1d68de70b4f2c281cbfaa4da3b6f
eyuuab <eyobedabreham9@gmail.com>
2025-01-12 22:34:43 +0300

    feat: featching user

--- client/src/pages/signup/signup.tsx
+++ client/src/pages/signup/signup.tsx
@@ -1,16 +1,19 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { EyeOff, Eye } from "lucide-react";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const [message, setMessage] = useState("");
   const [loading, setLoading] = useState(false);
 
-  //const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
+  // const API_URL = process.env.REACT_APP_API_URL || "http://127.0.0.1:8000";
 
   const handleSubmit = async (e: React.FormEvent) => {
-    e.preventDefault();
+    e.preventDefault();  
+
     setLoading(true);
     setMessage("");
 
@@ -19,10 +22,11 @@ const SignUp = () => {
         email: email,
         password: password,
       });
-      setMessage("Account created successfully");
+      setMessage(response.data.msg || 'Account created successfully');
       setEmail("");
       setPassword("");
-    } catch (error:any) {
+    } catch (error: any) {
+      console.error("Error response:", error.response);
       setMessage(
         error.response?.data?.detail || "Something went wrong, please try again."
       );
@@ -36,7 +40,7 @@ const SignUp = () => {
       <div className="bg-card p-8 rounded-lg shadow-lg max-w-md w-full transform transition duration-300 hover:scale-105">
         <h2 className="text-2xl font-bold text-primary mb-2 text-center">Join CoMakE</h2>
         <p className="text-sm text-center mb-6">Create an account to collaborate and build together!</p>
-        
+
         {message && (
           <div className="mb-4 text-center text-sm text-red-500">
             {message}
@@ -52,27 +56,41 @@ const SignUp = () => {
             onChange={(e) => setEmail(e.target.value)}
             required
           />
-          <input
-            type="password"
-            placeholder="Password"
-            className="w-full p-3 rounded-md bg-gray-800 text-text placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-primary transition duration-300"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
+          <div className="relative">
+            <input
+              type = {showPassword ? "text" : "password"}
+              placeholder = 'Password'
+              className="w-full p-3 rounded-md bg-gray-800 text-text placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-primary transition duration-300"
+              value = {password}
+              onChange = {(e) => setPassword(e.target.value)}
+              required         
+            />
+            <button
+              type="button"
+              className="absolute inset-y-0 right-3 flex items-center text-gray-500 hover:text-gray-300 focus:outline-none"
+              onClick={() => setShowPassword((prev) => !prev)} // Toggle visibility
+            >
+              {showPassword ? <Eye size={20} /> : <EyeOff size={20} />}
+            </button>
+
+          </div>
           <div className="flex justify-center">
             <button
               type="submit"
               className="w-32 p-2 rounded-md text-white border border-blue-300 hover:bg-blue-700 hover:border-blue-500 transition duration-300 focus:outline-none focus:ring-2 focus:ring-blue-400"
               disabled={loading}
             >
-              {loading ? "Signing Up..." : "Sign Up"}
+              {loading ? (
+                <div className="w-5 h-5 border-4 border-t-transparent border-white rounded-full animate-spin"></div>
+              ) : (
+                "Sign Up"
+              )}
             </button>
           </div>
         </form>
 
         <p className="text-sm text-center mt-4">
-          Already have an account?{' '}
+          Already have an account?{" "}
           <a href="/login" className="text-blue-300 text-primary hover:underline">
             Log In
           </a>
